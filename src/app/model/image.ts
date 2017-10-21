659848b40f72560eaaab4f01872f02880ce39843
Giyom <guillaume.larre@gmail.com>
2017-10-21 15:06:37 -0400

    Auto Login GB + photos consult & response 2. Add missing Files

---
+++ src/app/model/image.ts
@@ -0,0 +1,6 @@
+export class Image {
+  id: number;
+  uri: string;
+  description: string;
+  consultation_id: number;
+}
---
+++ src/providers/image.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Image } from '../app/model/image';
+
+declare var cordova: any;
+
+@Injectable()
+export class ImageService {
+
+  private imagesUrl = 'api/images';  // URL to web api
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+
+  constructor(private http: Http) { }
+
+  getImages(): Promise<Image[]> {
+    return this.http.get(this.imagesUrl)
+      .toPromise()
+      .then(response => response.json().data as Image[])
+      .catch(this.handleError);
+  }
+
+  getImage(id: number): Promise<Image> {
+    const url = `${this.imagesUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as Image)
+      .catch(this.handleError);
+  }
+
+  getImagesByConsultationId( consultation_id: number): Promise<Image[]>{
+  const url = `${this.imagesUrl}/?consultation_id=${consultation_id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as Image[])
+      .catch(this.handleError);
+
+  }
+
+  update(image: Image): Promise<Image>{
+    const url = `${this.imagesUrl}/${image.id}`;
+    console.log("image.update" + JSON.stringify(image));
+    return this.http
+      .put(url, JSON.stringify(image), {headers: this.headers})
+      .toPromise()
+      .then(() => image)
+      .catch(this.handleError);
+  }
+
+  createImage(image: Image): Promise<Image>{
+    return this.http.post(this.imagesUrl, JSON.stringify(image), this.headers)
+      .toPromise()
+      .then(response => response.json().data as Image)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+  public pathForImage(img) {
+    if (img === null) {
+      return '';
+    } else {
+      console.log(cordova.file.dataDirectory + img);
+      return cordova.file.dataDirectory + img;
+    }
+  }
+
+}
